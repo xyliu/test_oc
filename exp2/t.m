d3a5373057b88706c90a4fe4161287649f1122c5
Xinyun Liu <xinyunliu@gmail.com>
2013-09-29 23:46:35 +0800

    update test case of class

--- exp2/t.m
+++ exp2/t.m
@@ -26,21 +26,33 @@
 @end
 
 
+
+
 int main(int argc, char *argv[])
 {
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
 	Fraction *myFraction;
 
+	Fraction *myFraction2 = [[Fraction alloc] init];
+
 	myFraction = [Fraction alloc];
-	myFraction = [myFraction init];
+	myFraction = [myFraction init]; // exception with following try
+	//myFraction = [Fraction init];
+	//: Uncaught exception NSInvalidArgumentException, reason: Fraction(class) does not recognize setNumerator:
 
 	[myFraction setNumerator: 1];
 	[myFraction setDenominator: 3];
 
+	[myFraction2 setNumerator: 2];
+	[myFraction2 setDenominator: 5];
+
 	NSLog(@"The value of myFraction is:");
 	[myFraction print];
 	[myFraction release];
+
+	[myFraction2 print];
+	[myFraction2 release];
 
 	[pool drain];
 	return 0;
