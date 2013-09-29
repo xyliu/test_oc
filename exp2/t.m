125fbaa7b613d58db0b5fef4aec1c0d5747094d2
Xinyun Liu <xinyunliu@gmail.com>
2013-09-29 23:28:41 +0800

    first class

--- exp2/t.m
+++ exp2/t.m
@@ -1,12 +1,47 @@
 #import <Foundation/Foundation.h>
+
+@interface Fraction: NSObject
+{
+	int numerator;
+	int denominator;
+}
+-(void)	print;
+-(void) setNumerator: (int) n;
+-(void) setDenominator: (int) d;
+@end
+
+@implementation Fraction
+-(void) print
+{
+	NSLog(@"%i/%i", numerator, denominator);
+}
+-(void) setNumerator: (int) n;
+{
+	numerator = n;
+}
+-(void) setDenominator: (int) d;
+{
+	denominator = d;
+}
+@end
+
+
 int main(int argc, char *argv[])
 {
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	int numerator = 1;
-	int denominator = 3;
-	NSLog(@"the fractor is %i/%i", numerator, denominator);
+
+	Fraction *myFraction;
+
+	myFraction = [Fraction alloc];
+	myFraction = [myFraction init];
+
+	[myFraction setNumerator: 1];
+	[myFraction setDenominator: 3];
+
+	NSLog(@"The value of myFraction is:");
+	[myFraction print];
+	[myFraction release];
 
 	[pool drain];
-
 	return 0;
 }
